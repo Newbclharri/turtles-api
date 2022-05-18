02b4729f9f194329f3ad5f75d41dfe1a7ebeafdb
contactharrisc2 <contactharrisc2@gmail.com>
2022-05-18 00:42:33 -0500

    adds app.use(cors())

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 // MIDDLEWARE
 /////////////////////
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 //////////////////
 // ROUTES
 /////////////////
